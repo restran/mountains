5ded983111171ccecd7eebe9e56b2b1e2ff488b4
restran <grestran@gmail.com>
2018-03-24 20:52:13 +0800

    string_utils: is empty

--- mountains/utils/string_utils.py
+++ mountains/utils/string_utils.py
@@ -17,5 +17,26 @@ def fixed_length_split(s, width):
 
 
 def line_break(s, length=76):
+    """
+    将字符串分割成一行一行
+    :param s:
+    :param length:
+    :return:
+    """
     x = '\n'.join(s[pos:pos + length] for pos in range(0, len(s), length))
     return x
+
+
+def is_empty(s):
+    if s == '' or s is None or not isinstance(s, str):
+        return True
+    else:
+        return False
+
+
+def any_empty(*params):
+    for s in params:
+        if is_empty(s):
+            return True
+    else:
+        return False
